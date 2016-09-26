4cccf8212ba8cf8bebca101d20951267f606e01e
Patrick Shuff <patrick.shuff@gmail.com>
2016-09-25 19:55:23 -0700

    Adding a few more unit tests

--- tests/artofmemory_tests.py
+++ tests/artofmemory_tests.py
@@ -1,25 +1,26 @@
-from artofmemory import convert_word_to_major
+from artofmemory import (
+    convert_word_to_major,
+    build_regex_from_letter_mapping,
+)
+
+# Here is a stub major mapping so testing is consistent
+default_major_map = {0: ['s', 'z'],
+                        1: ['t', 'd'],
+                        2: ['n'],
+                        3: ['m'],
+                        4: ['r'],
+                        5: ['l'],
+                        6: ['j', 'g'],
+                        7: ['c', 'k', 'q'],
+                        8: ['v', 'f'],
+                        9: ['p', 'b']}
+
 
 import unittest
 
 class TestArtOfMemory(unittest.TestCase):
     '''
-    Here is my adapted major system
-
-    Digit   Letter
-    0   s, z
-    1   t, d
-    2   n
-    3   m
-    4   r
-    5   l
-    6   j, g
-    7   c, k, q, 
-    8   v, f
-    9   p, b
-
-    e.g. satellite => 0151
-
+    Test all the things in artofmemory.py
     '''
     def test_ts(self):
         ret = convert_word_to_major('test')
@@ -48,6 +49,14 @@ class TestArtOfMemory(unittest.TestCase):
     def test_word_with_double_letter(self):
         ret = convert_word_to_major('basketball')
         self.assertEqual(ret, '9071955')
+
+    def test_regex_builder_empty(self):
+        ret = build_regex_from_letter_mapping(dict())
+        self.assertEqual(ret, '()')
+
+    def test_regex_builder_full(self):
+        ret = build_regex_from_letter_mapping(default_major_map)
+        self.assertEqual(ret, '(s|z|t|d|n|m|r|l|j|g|c|k|q|v|f|p|b)')
 
 if __name__ == '__main__':
     unittest.main()
